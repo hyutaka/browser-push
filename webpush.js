c28fad3cfcf89ffa1414786e8e311bafae8a3627
Watanabe <hyutaka@gmail.com>
2022-05-20 10:00:32 -0300

    Options added

--- webpush.js
+++ webpush.js
@@ -14,11 +14,9 @@ webpush.setVapidDetails(
 
 // This is the same output of calling JSON.stringify on a PushSubscription
 const pushSubscription = {
-  endpoint: '.....',
-  keys: {
-    auth: '.....',
-    p256dh: '.....'
-  }
-};
+  "endpoint":"https://fcm.googleapis.com/fcm/send/dEt-NEgqwLo:APA91bELCxVtsjOf3oV49s70RQ9KORcvHDvQgdSivOkC1pkymQ71axYCyj60PlyJYxSG1l9OePkS6ogjZ23Wb9XvIr4X3o6-99iLf-tYe1SCCVrI8sCm4W08KUzotrni2Ox_Y_P5w_-g",
+  "expirationTime":null,
+  "keys":{"p256dh":"BH8acNcfD3pt7MH4svmw9J9xcTWNYPWNPP1K44R1CPGdM31g57TR6TKxIaW9a9CdpRK7KY_rj6QYa6jXRLzNS_A","auth":"G2RB3PG8EnTMXOUS59K5qQ"}}
+;
 
 webpush.sendNotification(pushSubscription, 'Your Push Payload Text');

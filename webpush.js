70fdd4c6dde719bba996cf61d65cf4f077d13bd9
Watanabe <hyutaka@gmail.com>
2022-05-20 17:00:41 -0300

    Static notification

--- webpush.js
+++ webpush.js
@@ -13,14 +13,5 @@ webpush.setVapidDetails(
 );
 
 // This is the same output of calling JSON.stringify on a PushSubscription
-const pushSubscription = {
-  "endpoint": "https://fcm.googleapis.com/fcm/send/fo9xRYrgzvE:APA91bEVq9beB3IbPOePO2krqbjoqnd8De1gqNYRq1MgXmxN_NpT9wrj8V47BzU9dxgiGvilnWULnmypZXh1RHwLftw-A8OekYljlcdn_cdRH_lFKQc_DffBc7j8_wFcJV81eX09T312",
-  "expirationTime": null,
-  "keys": {
-    "p256dh": "BMY1wod1eL0yYBc7GVoN_cgh82YJ6DiVh9awbE-AqZDWW2qa1sHBopUx34nmYxIy0QW6QyVpmM-NRDMHP9qB6Ls",
-    "auth": "mUyA1kqckephKfOu2P0XYA"
-  }
-}
-  ;
-
-webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
+const pushSubscription = {"endpoint":"https://fcm.googleapis.com/fcm/send/fM4OS4htAVk:APA91bHYej76mViN0JMp22BFbc6vAsLy8cwDuANG7bp7Rox8p3PhRWUWyUSCExVO2uoVPwWpkQoaqvbMqG4-_aGW8_fa4s2NExTqWD7y0qJ3kvuTtsnkAE38hELd5tiHBHgUSoVk7JR8","expirationTime":null,"keys":{"p256dh":"BChAOCHbm_LzzIAOz_ck3IFzTVJOsE2s4whtsbBPyYXAB4dTJ6TX2qw0cR2FJePS-imvwn23DG_5AWddngZ_FRQ","auth":"z1xOBiOOTa9fvZ2WNzfkNg"}};
+  webpush.sendNotification(pushSubscription, 'Your Push Payload Text');

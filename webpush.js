127256a715c3c0150a0fed7e7cc95ddfc5cbc2e1
Watanabe <hyutaka@gmail.com>
2022-05-20 10:50:57 -0300

    New keys

--- webpush.js
+++ webpush.js
@@ -2,9 +2,9 @@ const webpush = require('web-push');
 
 // VAPID keys should only be generated only once.
 const vapidKeys = {
-  publicKey: 'BFrFoDGk-dPJ-3b71p3rmf9HaqsbtSx_a8X0Ue7wuMdq9nziNCFtiqCXVqL9CtFgoeQ5KD7pyTYhOen2uF3qLUw',
-  privateKey: 'OGmmtAoQoOQ2h4rfEptii1-6-ShlKDGA4fId4n2jYTU'
-};
+    publicKey: 'BBytYAQJ75ZNC9Xsf8rMdYMKojpqY43L5A6tfFaVX2zhP5gC30XYTnlwKw4pbxcbmjlVTEEo3vdDnMZX3IPEA1Y',
+    privateKey: 'WPSgnOQpF4JlIm2Ss6ZVtYFBmDiUgm4YtHF6H-1fo60'
+  };
 
 webpush.setVapidDetails(
   'mailto:example@yourdomain.org',
@@ -14,9 +14,13 @@ webpush.setVapidDetails(
 
 // This is the same output of calling JSON.stringify on a PushSubscription
 const pushSubscription = {
-  "endpoint":"https://fcm.googleapis.com/fcm/send/dEt-NEgqwLo:APA91bELCxVtsjOf3oV49s70RQ9KORcvHDvQgdSivOkC1pkymQ71axYCyj60PlyJYxSG1l9OePkS6ogjZ23Wb9XvIr4X3o6-99iLf-tYe1SCCVrI8sCm4W08KUzotrni2Ox_Y_P5w_-g",
-  "expirationTime":null,
-  "keys":{"p256dh":"BH8acNcfD3pt7MH4svmw9J9xcTWNYPWNPP1K44R1CPGdM31g57TR6TKxIaW9a9CdpRK7KY_rj6QYa6jXRLzNS_A","auth":"G2RB3PG8EnTMXOUS59K5qQ"}}
-;
+  "endpoint": "https://fcm.googleapis.com/fcm/send/fo9xRYrgzvE:APA91bEVq9beB3IbPOePO2krqbjoqnd8De1gqNYRq1MgXmxN_NpT9wrj8V47BzU9dxgiGvilnWULnmypZXh1RHwLftw-A8OekYljlcdn_cdRH_lFKQc_DffBc7j8_wFcJV81eX09T312",
+  "expirationTime": null,
+  "keys": {
+    "p256dh": "BMY1wod1eL0yYBc7GVoN_cgh82YJ6DiVh9awbE-AqZDWW2qa1sHBopUx34nmYxIy0QW6QyVpmM-NRDMHP9qB6Ls",
+    "auth": "mUyA1kqckephKfOu2P0XYA"
+  }
+}
+  ;
 
 webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
